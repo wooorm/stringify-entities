967376fbb6da3991805a99c369e2d952a677a366
Titus Wormer <tituswormer@gmail.com>
2021-03-27 11:40:37 +0100

    Add JSDoc based types

--- build.js
+++ build.js
@@ -6,19 +6,21 @@ import {characterEntities} from 'character-entities'
 var legacy = Object.keys(characterEntitiesLegacy)
 var entities = Object.keys(characterEntities)
 
+/** @type {string[]} */
 var conflict = []
-var length = legacy.length
-var count = entities.length
 var index = -1
+/** @type {number} */
 var offset
+/** @type {string} */
 var left
+/** @type {string} */
 var right
 
-while (++index < length) {
+while (++index < legacy.length) {
   left = legacy[index]
   offset = -1
 
-  while (++offset < count) {
+  while (++offset < entities.length) {
     right = entities[offset]
 
     if (left !== right && right.slice(0, left.length) === left) {
